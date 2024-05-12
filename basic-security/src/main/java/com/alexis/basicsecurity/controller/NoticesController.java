e6edb13dad3d6e467f2160f3ce141ad4331de1a9
AlexisUrbainsky <aurbainsky@gmail.com>
2024-05-12 00:37:19 -0300

    adding db configuration and  using basic authorization in different routes, 4 requires authorization and 2 doesn't

---
+++ basic-security/src/main/java/com/alexis/basicsecurity/controller/NoticesController.java
@@ -0,0 +1,15 @@
+package com.alexis.basicsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notices")
+public class NoticesController {
+
+    @GetMapping()
+    public String getNoticesDetails(){
+        return "Very soon you are going to have notices of us";
+    }
+}
