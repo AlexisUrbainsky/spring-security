40dd0cf556e339dae6fe437c1a2cfcf5955c84ab
AlexisUrbainsky <aurbainsky@gmail.com>
2024-06-22 21:17:01 -0300

    fix errors, adding JWT Token dependencies, configuration, and filters

--- jwt/src/main/java/com/alexis/jwt/config/ProjectSecurityConfig.java
+++ jwt/src/main/java/com/alexis/jwt/config/ProjectSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.alexis.jwt.config;
 
-import com.alexis.jwt.filter.AuthoritiesLoggingAfterFilter;
-import com.alexis.jwt.filter.AuthoritiesLoggingAtFilter;
-import com.alexis.jwt.filter.CsrfCookieFilter;
-import com.alexis.jwt.filter.RequestValidationBeforeFilter;
+import com.alexis.jwt.filter.*;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -19,11 +17,11 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
-
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -31,25 +29,27 @@ public class ProjectSecurityConfig {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
 
-        http.securityContext((context) -> context.requireExplicitSave(false))
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                config.setAllowedMethods(Collections.singletonList("*"));
-                config.setAllowCredentials(true);
-                config.setAllowedHeaders(Collections.singletonList("*"));
-                config.setMaxAge(3600L);
-                return config;
-            }
-        })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact", "/login/*")
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+        @Override
+        public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setExposedHeaders(Arrays.asList("Authorization"));
+            config.setMaxAge(3600L);
+            return config;
+        }
+        })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact", "/register")
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                         .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                         .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
                         .addFilterAt(new AuthoritiesLoggingAtFilter(), BasicAuthenticationFilter.class)
                         .addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
+                        .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+                        .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
                         .authorizeHttpRequests((requests)->requests
                         /*.requestMatchers("/account").hasAuthority("VIEWACCOUNT")
                         .requestMatchers("/balance").hasAnyAuthority("VIEWACCOUNT","VIEWBALANCE")
@@ -60,7 +60,7 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/loans").hasRole("USER")
                         .requestMatchers("/cards").hasRole("XUSER")
                         //.requestMatchers("/user").authenticated()
-                .requestMatchers("/notices","/contact","/login/*").permitAll())
+                .requestMatchers("/notices","/contact","/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
--- jwt/src/main/java/com/alexis/jwt/controller/LoginController.java
+++ jwt/src/main/java/com/alexis/jwt/controller/LoginController.java
@@ -5,13 +5,16 @@ import com.alexis.jwt.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/login")
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class LoginController {
 
@@ -40,6 +43,15 @@ public class LoginController {
         return response;
     }
 
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        Optional<Customer> customers = customerRepository.findByEmail(authentication.getName());
+        if (!customers.isEmpty() ) {
+            return customers.get();
+        } else {
+            return null;
+        }
 
+    }
 
 }
